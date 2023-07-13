0fbe481dd0187874001e4d3753ebd29dde397017
carlos1377 <carloseduardoorso1@gmail.com>
2023-07-12 21:22:05 -0300

    ajustando a cagada

--- aula28.py
+++ aula28.py
@@ -1,1 +1,14 @@
-print('ashdishudsa')
+"""
+Introdução ao try/except
+try -> tentar executar o código
+except -> ocorreu algum erro ao tentar executar
+"""
+
+numero_str = input('digite um número: ')
+
+try:
+    print('str:', numero_str)
+    numero_float = float(numero_str)
+    print(f'O dobro de {numero_str} é {numero_float *2:.2f}')
+except:
+    print('isso não é um número')
