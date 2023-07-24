7c89ad0b602bf2f1efe824577c19d19d608d734b
TI01 <ti01@multimoveis.local>
2023-07-24 11:11:28 -0300

    aula 33 y aula34

---
+++ aula33.py
@@ -0,0 +1,10 @@
+"""
+Repetições
+while(enquanto)
+Executa uma ação enquanto uma condição fro verdadeira
+"""
+
+condicao = True
+while condicao:
+    nome = input('qual seu nome? ')
+    print(f'seu nome é {nome}')
