275c135be88655aecccd0647b35e2d1a991ce181
TI01 <ti01@multimoveis.local>
2023-10-18 16:51:52 -0300

    dotenv, random e secret

---
+++ main_folder/aula182.py
@@ -0,0 +1,59 @@
+# secrets gera números aleatórios seguros
+from secrets import SystemRandom as Sr
+import secrets
+import string as s
+
+# python -c "import string as s;from secrets import SystemRandom as Sr; print(''.join(Sr().choices(s.ascii_letters + s.punctuation + s.digits,k=12)))"
+
+random = secrets.SystemRandom()
+# usar o random dessa forma o deixa mais seguro e sem uma seed sendo possível
+# para saber qual será a randomização
+
+# print(secrets.randbelow(100))
+# print(random.choice([99, 10, 345, 324, 11111,]))
+
+# print(s.ascii_letters)
+# print(s.digits)
+# print(s.punctuation)
+
+print(''.join(Sr().choices(s.ascii_letters + s.digits + s.punctuation, k=12)))
+
+# Funções:
+# seed
+#   -> NÃO FAZ NADA
+random.seed(10)
+
+# random.randrange(início, fim, passo)
+#   -> Gera um número inteiro aleatório dentro de um intervalo específico
+r_range = random.randrange(10, 20, 2)
+print(r_range)
+
+# random.randint(início, fim)
+#   -> Gera um número inteiro aleatório dentro de um intervalo "sem passo"
+r_int = random.randint(10, 20)
+print(r_int)
+
+# random.uniform(início, fim)
+#   -> Gera um número flutuante aleatório dentro de um intervalo "sem passo"
+r_uniform = random.uniform(10, 20)
+print(r_uniform)
+
+# random.shuffle(SequenciaMutável) -> Embaralha a lista original
+nomes = ['Luiz', 'Maria', 'Helena', 'Joana']
+# random.shuffle(nomes)
+print(nomes)
+
+# random.sample(Iterável, k=N)
+#   -> Escolhe elementos do iterável e retorna outro iterável (não repete)
+novos_nomes = random.sample(nomes, k=3)
+print(nomes)
+print(novos_nomes)
+
+# random.choices(Iterável, k=N)
+#   -> Escolhe elementos do iterável e retorna outro iterável (repete valores)
+novos_nomes = random.choices(nomes, k=3)
+print(nomes)
+print(novos_nomes)
+
+# random.choice(Iterável) -> Escolhe um elemento do iterável
+print(random.choice(nomes))
